bb3498f1828c9ef4c52e5f6061793e84864cb1bc
JWei <jay0935199@gmail.com>
2026-05-19 14:25:31 +0800

    ci: 升級 actions runtime 至 Node 24，允許 void element 自閉合寫法

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,10 +1,16 @@
-// @ts-check
-import withNuxt from './.nuxt/eslint.config.mjs'
+/** @format */
 
-export default withNuxt(
-  {
-    rules: {
-      'vue/multi-word-component-names': 'off',
-    },
+// @ts-check
+import withNuxt from './.nuxt/eslint.config.mjs';
+
+export default withNuxt({
+  rules: {
+    'vue/multi-word-component-names': 'off',
+    'vue/html-self-closing': [
+      'error',
+      {
+        html: { void: 'always', normal: 'always', component: 'always' },
+      },
+    ],
   },
-)
+});
